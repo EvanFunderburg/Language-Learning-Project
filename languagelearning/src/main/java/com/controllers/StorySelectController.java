040adfea9ac2fa04a6cce11003be38fa6ff36ab9
Will Thacher <wthacher@email.sc.edu>
2024-12-05 08:43:33 -0800

    idk

--- languagelearning/src/main/java/com/controllers/StorySelectController.java
+++ languagelearning/src/main/java/com/controllers/StorySelectController.java
@@ -1,5 +1,20 @@
 package com.controllers;
 
+import java.io.IOException;
+
+import com.language.App;
+import com.model.LanguageAppFacade;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class StorySelectController {
-    
+    @FXML
+    void switchToUserHomePage() throws IOException {
+        App.setRoot("primary");
+    }
+    @FXML
+    void switchToStoryPage() throws IOException {
+        App.setRoot("primary");
+    }
 }

85fa0352c90d16c776e714fe1e9f7b0fe270fa2c
Will Thacher <wthacher@email.sc.edu>
2024-12-05 13:49:58 -0800

    story stuff

--- languagelearning/src/main/java/com/controllers/StoryPageController.java
+++ languagelearning/src/main/java/com/controllers/StoryPageController.java
@@ -1,5 +1,18 @@
 package com.controllers;
 
+import java.io.IOException;
+
+import com.language.App;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class StoryPageController {
+    @FXML private Button homepage;
+    @FXML
+    void switchToUserHomePage(ActionEvent event) throws IOException {
+        App.setRoot("primary");
+    }
     
 }
